b0aff3ad2d6ae299010e27b96db3a494bd626809
ChrisM345 <chrism.12277@gmail.com>
2024-08-27 19:31:17 -0500

    Added options for searching

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,20 @@
-const { getPokemonData } = require("../db/queries");
+const { getPokemonData, searchPokemonName, searchPokemonType } = require("../db/queries");
 
 module.exports = {
   get: async (req, res) => {
-    pokemonData = await getPokemonData();
+    const pokemonData = await getPokemonData();
     res.render("indexView", { title: "Pokedex", data: pokemonData });
+  },
+
+  getSearchName: async (req, res) => {
+    const searchTerm = req.query.pokemonName.toLowerCase();
+    const pokemonData = await searchPokemonName(searchTerm);
+    res.render("indexView", { title: "Pokedex Search Results", data: pokemonData, search: searchTerm });
+  },
+
+  getSearchType: async (req, res) => {
+    const searchTerm = req.query.pokemonType.toLowerCase();
+    const pokemonData = await searchPokemonType(searchTerm);
+    res.render("indexView", { title: "Pokedex Search Results", data: pokemonData, search: searchTerm });
   },
 };
