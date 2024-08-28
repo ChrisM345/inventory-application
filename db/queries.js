1734466d4668807d223203e98096f89f479e2b8e
ChrisM345 <chrism.12277@gmail.com>
2024-08-27 19:31:53 -0500

    Added additional queries

--- db/queries.js
+++ db/queries.js
@@ -5,6 +5,24 @@ async function getPokemonData() {
   return rows;
 }
 
+async function getPokemonDetails(pokemonID) {
+  const { rows } = await pool.query(`SELECT * FROM pokemon WHERE id=${pokemonID}`);
+  return rows;
+}
+
+async function searchPokemonName(name) {
+  const { rows } = await pool.query(`SELECT * FROM pokemon WHERE name LIKE '%${name}%'`);
+  return rows;
+}
+
+async function searchPokemonType(type) {
+  const { rows } = await pool.query(`SELECT * FROM pokemon WHERE types LIKE '%${type}%'`);
+  return rows;
+}
+
 module.exports = {
   getPokemonData,
+  getPokemonDetails,
+  searchPokemonName,
+  searchPokemonType,
 };
