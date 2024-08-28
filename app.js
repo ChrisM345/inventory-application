1f8bb742a9127f2d06206d12eea808ac12b9d40c
ChrisM345 <chrism.12277@gmail.com>
2024-08-27 19:33:58 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const indexRoute = require("./routes/indexRoute");
+const pokemonRoute = require("./routes/pokemonRoute");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 app.use("/", indexRoute);
+app.use("/pokemon/", pokemonRoute);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
