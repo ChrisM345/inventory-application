84c5a5481020d8f4782d653112e68cd7f6f520a8
ChrisM345 <chrism.12277@gmail.com>
2024-09-03 19:56:49 -0500

    Added queries

--- db/queries.js
+++ db/queries.js
@@ -10,6 +10,11 @@ async function getPokemonDetails(pokemonID) {
   return rows;
 }
 
+async function getPokemonImgURL(name) {
+  const { rows } = await pool.query(`SELECT img_url FROM pokemon WHERE name='${name}'`);
+  return rows[0].img_url;
+}
+
 async function searchPokemonName(name) {
   const { rows } = await pool.query(`SELECT * FROM pokemon WHERE name LIKE '%${name}%'`);
   return rows;
@@ -20,17 +25,52 @@ async function searchPokemonType(type) {
   return rows;
 }
 
-async function addTrainer(name, pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6) {
+async function getTrainers() {
+  const { rows } = await pool.query(`SELECT * FROM trainers ORDER BY id`);
+  return rows;
+}
+
+async function addTrainer(
+  name,
+  pokemon1,
+  pokemon1URL,
+  pokemon2,
+  pokemon2URL,
+  pokemon3,
+  pokemon3URL,
+  pokemon4,
+  pokemon4URL,
+  pokemon5,
+  pokemon5URL,
+  pokemon6,
+  pokemon6URL
+) {
   await pool.query(
-    "INSERT INTO trainers (name, pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6) VALUES ($1, $2, $3, $4, $5, $6, $7)",
-    [name, pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6]
+    "INSERT INTO trainers (name, pokemon1, pokemon1URL, pokemon2, pokemon2URL, pokemon3, pokemon3URL, pokemon4, pokemon4URL, pokemon5, pokemon5URL, pokemon6, pokemon6URL) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+    [
+      name,
+      pokemon1,
+      pokemon1URL,
+      pokemon2,
+      pokemon2URL,
+      pokemon3,
+      pokemon3URL,
+      pokemon4,
+      pokemon4URL,
+      pokemon5,
+      pokemon5URL,
+      pokemon6,
+      pokemon6URL,
+    ]
   );
 }
 
 module.exports = {
   getPokemonData,
   getPokemonDetails,
+  getPokemonImgURL,
   searchPokemonName,
   searchPokemonType,
+  getTrainers,
   addTrainer,
 };
