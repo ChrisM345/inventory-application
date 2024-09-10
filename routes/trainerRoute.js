a61419ab30ce85049d447fee196fa04c09e0defb
ChrisM345 <chrism.12277@gmail.com>
2024-09-09 21:17:07 -0500

    Added routes for editing and deleting trainers

--- routes/trainerRoute.js
+++ routes/trainerRoute.js
@@ -5,5 +5,8 @@ const controller = require("../controllers/trainerController");
 router.get("/", controller.get);
 router.get("/createTrainer", controller.getCreateTrainer);
 router.post("/createTrainer", controller.postCreateTrainer);
+router.get("/:trainerID/edit", controller.getEditTrainer);
+router.post("/:trainerID/edit", controller.postEditTrainer);
+router.post("/:trainerID/delete", controller.postDeleteTrainer);
 
 module.exports = router;
